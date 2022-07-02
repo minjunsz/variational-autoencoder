022d29c5363450be5072b33fff0809bf718fcc34
minjunsz <minjunsz@postech.ac.kr>
2022-07-02 23:06:51 +0200

    introduce lr_scheduler & KL annealing

--- models/naive_vae.py
+++ models/naive_vae.py
@@ -1,4 +1,5 @@
 # %%
+# from torch.optim.lr_scheduler import ReduceLROnPlateau
 from dataclasses import dataclass
 from typing import Optional, Tuple
 
@@ -8,7 +9,7 @@ import torch
 import torch.nn.functional as F
 from pytorch_lightning.utilities.types import STEP_OUTPUT
 from torch import nn
-from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.optim.lr_scheduler import StepLR
 
 from .types import TorchDevice, TorchTensor
 
@@ -52,7 +53,7 @@ class NaiveVAE(pl.LightningModule):
         hidden_dim: list[int],
         latent_dim: int,
         lr: float = 5e-4,
-        **kwargs
+        **kwargs,
     ) -> None:
         super().__init__()
         self.save_hyperparameters()
@@ -100,6 +101,15 @@ class NaiveVAE(pl.LightningModule):
             nn.Tanh(),
         )
 
+    @property
+    def kl_weight(self) -> float:
+        if self.current_epoch < 3:
+            return 0
+        elif self.current_epoch < 5:
+            return 0.001
+        else:
+            return 0.01
+
     def encode(self, input: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         hidden: torch.Tensor = self.encode_blocks(input)
         hidden = hidden.flatten(start_dim=1)
@@ -142,31 +152,36 @@ class NaiveVAE(pl.LightningModule):
         output = self.forward(x)
         recon_loss, kld_loss = self.loss_function(x, output)
         scheduler = self.lr_schedulers()
-
+        total_loss = recon_loss + self.kl_weight * kld_loss
         log_values = {
-            "train_total_loss": recon_loss.detach() + kld_loss.detach(),
+            "train_total_loss": total_loss.detach(),
             "train_reconstruction_loss": recon_loss.detach(),
             "train_KL_divergence": kld_loss.detach(),
+            "learning_rate": scheduler.get_last_lr()[0],
         }
         self.log_dict(log_values)
-        scheduler.step(recon_loss.detach() + kld_loss.detach())
 
-        return {"loss": recon_loss + kld_loss}
+        # scheduler.step()
+
+        return {"loss": total_loss}
 
     def validation_step(self, batch, batch_idx: int) -> Optional[STEP_OUTPUT]:
         # this is the test loop
         x, y = batch
         output = self.forward(x)
         recon_loss, kld_loss = self.loss_function(x, output)
+        total_loss = recon_loss + self.kl_weight * kld_loss
 
         if batch_idx == 0:
             original = x[0:5]
             recons = output.reconstructed[0:5]
+            gen = self.generate_img(5)
             self.logger.log_image(key="val_original", images=[original])
             self.logger.log_image(key="val_reconstructed", images=[recons])
+            self.logger.log_image(key="val_generated", images=[gen])
 
         log_values = {
-            "val_total_loss": recon_loss.detach() + kld_loss.detach(),
+            "val_total_loss": total_loss.detach(),
             "val_reconstruction_loss": recon_loss.detach(),
             "val_KL_divergence": kld_loss.detach(),
         }
@@ -177,6 +192,7 @@ class NaiveVAE(pl.LightningModule):
         x, y = batch
         output = self.forward(x)
         recon_loss, kld_loss = self.loss_function(x, output)
+        total_loss = recon_loss + self.kl_weight * kld_loss
 
         if batch_idx == 0:
             wandb_logger = self.logger.experiment
@@ -186,7 +202,7 @@ class NaiveVAE(pl.LightningModule):
             self.logger.log_image(key="test_reconstructed", images=[recons])
 
         log_values = {
-            "test_total_loss": recon_loss.detach() + kld_loss.detach(),
+            "test_total_loss": total_loss.detach(),
             "test_reconstruction_loss": recon_loss.detach(),
             "test_KL_divergence": kld_loss.detach(),
         }
@@ -197,21 +213,23 @@ class NaiveVAE(pl.LightningModule):
         return {
             "optimizer": optimizer,
             "lr_scheduler": {
-                "scheduler": ReduceLROnPlateau(optimizer, "min"),
-                "monitor": "train_total_loss",
+                "scheduler": StepLR(optimizer, step_size=200, gamma=0.9),
             },
         }
 
-    def reconstruct_img(self, original: torch.Tensor) -> torch.Tensor:
+    def reconstruct_img(self, original: TorchTensor) -> TorchTensor:
         """
         :param x: (torch.Tensor) [B x C x H x W]
         :return: (torch.Tensor) [B x C x H x W]
         """
         return self.forward(original).reconstructed
 
-    def generate_img(self, num_imgs: int, device: torch.device) -> torch.Tensor:
+    def generate_img(
+        self, num_imgs: int, device: Optional[TorchTensor] = None
+    ) -> TorchTensor:
         latent = torch.randn(num_imgs, self.latent_dim)
-        latent.to(device=device)
+        if device is not None:
+            latent.to(device=device)
         return self.decode(latent)
 
 
